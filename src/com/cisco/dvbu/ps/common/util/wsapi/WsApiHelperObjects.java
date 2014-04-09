dba8e1430cb86d8cfc24bd0c656a1e8839897337
Michael DeAngelo <mideange@cisco.com>
2014-04-09 13:02:52 -0400

    fix file headers

--- src/com/cisco/dvbu/ps/common/util/wsapi/WsApiHelperObjects.java
+++ src/com/cisco/dvbu/ps/common/util/wsapi/WsApiHelperObjects.java
@@ -1,3 +1,13 @@
+/*******************************************************************************
+* Copyright (c) 2014 Cisco Systems
+* All rights reserved. This program and the accompanying materials
+* are made available under the terms of the Eclipse Public License v1.0
+* which accompanies this distribution, and is available at
+* http://www.eclipse.org/legal/epl-v10.html
+*
+* Contributors:
+* PDTool project commiters - initial release
+*******************************************************************************/
 /**
  * (c) 2014 Cisco and/or its affiliates. All rights reserved.
  */
