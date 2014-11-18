170929a5c5e69586fb11a955d3add9b65a890595
Mike Tinius <mtinius@cisco.com>
2014-11-18 10:44:37 -0500

    Release for 2014-11-18 with updated license

--- regression/setVars.bat
+++ regression/setVars.bat
@@ -1,6 +1,20 @@
 @echo off
 REM ######################################################################
 REM # (c) 2014 Cisco and/or its affiliates. All rights reserved.
+REM # 
+REM # This software is released under the Eclipse Public License. The details can be found in the file LICENSE. 
+REM # Any dependent libraries supplied by third parties are provided under their own open source licenses as 
+REM # described in their own LICENSE files, generally named .LICENSE.txt. The libraries supplied by Cisco as 
+REM # part of the Composite Information Server/Cisco Data Virtualization Server, particularly csadmin-XXXX.jar, 
+REM # csarchive-XXXX.jar, csbase-XXXX.jar, csclient-XXXX.jar, cscommon-XXXX.jar, csext-XXXX.jar, csjdbc-XXXX.jar, 
+REM # csserverutil-XXXX.jar, csserver-XXXX.jar, cswebapi-XXXX.jar, and customproc-XXXX.jar (where -XXXX is an 
+REM # optional version number) are provided as a convenience, but are covered under the licensing for the 
+REM # Composite Information Server/Cisco Data Virtualization Server. They cannot be used in any way except 
+REM # through a valid license for that product.
+REM # 
+REM # This software is released AS-IS!. Support for this software is not covered by standard maintenance agreements with Cisco. 
+REM # Any support for this software by Cisco would be covered by paid consulting agreements, and would be billable work.
+REM # 
 REM ######################################################################
 REM ###################################################
 REM # Set customer variables
