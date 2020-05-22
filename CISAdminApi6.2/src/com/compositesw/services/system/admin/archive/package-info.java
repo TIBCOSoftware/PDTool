d4e7944249ec31f1ce135ebd9d163b9bd50ff39e
mtinius <mtinius@MTINIUS-X1E2.na.tibco.com>
2020-05-22 17:27:39 -0400

    Cleanup 6.2 code

--- CISAdminApi6.2/src/com/compositesw/services/system/admin/archive/package-info.java
+++
@@ -1,2 +0,0 @@
-@javax.xml.bind.annotation.XmlSchema(namespace = "http://www.compositesw.com/services/system/admin/archive", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
-package com.compositesw.services.system.admin.archive;
--- CISAdminApi6.2/src/com/compositesw/services/system/admin/resource/package-info.java
+++
@@ -1,2 +0,0 @@
-@javax.xml.bind.annotation.XmlSchema(namespace = "http://www.compositesw.com/services/system/admin/resource", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
-package com.compositesw.services.system.admin.resource;
